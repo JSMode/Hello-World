fe9cc7fe9f543c15e6bbc97ebeecfc39cbe8a171
JSMode <jamessimonsen5@gmail.com>
2017-01-10 23:08:02 -0800

    Update 2buttons.py

--- 2buttons.py
+++ 2buttons.py
@@ -1,5 +1,12 @@
-# -*- coding: utf-8 -*-
+#
+# Simple 2 button setup for Raspberry Pi
+# buttons will repeat print if held down
+# refer to 2buttons-singlepress.py for 
+# single reading per push, regardless the
+# duration of button depression.
+# JSMode 2017
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(18, GPIO.IN)
@@ -8,4 +15,5 @@ while True:
         print “Button 1 pressed”
     if(GPIO.input(18) ==1):
         print “Button 2 pressed”
+    
 GPIO.cleanup()
